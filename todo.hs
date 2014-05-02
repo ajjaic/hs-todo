5e2f3c1b5b4f52c8f85267ec30b3976828796454
Aj <aj@aj.com>
2014-05-02 16:51:02 +0000

    added a few parsers to parse different parts of a task entry

--- todo.hs
+++ todo.hs
@@ -1,6 +1,10 @@
 import Data.Time.Clock
+import Data.Time.Format
+import System.Locale
+import Data.Attoparsec.Combinator
 import Data.Attoparsec.ByteString.Char8
 import qualified Data.ByteString.Char8 as B
+import qualified Data.List as L
 
 data Priority = A | B | C deriving (Show)
 data Task = Task { taskc :: String,
@@ -11,18 +15,40 @@ data Task = Task { taskc :: String,
                    ctxt :: Maybe [String] } deriving (Show)
 
 t = "(A) 2014-04-17 us doolars convert +home @weekend"
+p = "(A)"
 d = "2014-04-17"
+c = "us doolars convert"
+pr = "+home"
+ct = "@weekend"
+test = "us doolars convert +makerspace"
 
 parsePriority :: Parser Priority
-parsePriority = do char '('
-                   c <- satisfy $ inClass "ABC"
-                   char ')'
-                   return $ pri c
+parsePriority = do  char '('
+                    c <- satisfy $ inClass "ABC"
+                    char ')'
+                    return $ pri c
     where
-        pri c
-          | c == 'A' = A
-          | c == 'B' = B
-          | c == 'C' = C
+        pri 'A' = A
+        pri 'B' = B
+        pri 'C' = C
 
+parseDate :: Parser UTCTime
+parseDate = do date <- count 10 $ choice [digit, char '-']
+               return $ (readTime defaultTimeLocale "%Y-%m-%d" date :: UTCTime)
 
+parseProject :: Parser String
+parseProject = do char '+'
+                  prj <- many1 letter_ascii
+                  return prj
 
+parseContext :: Parser String
+parseContext = do char '@'
+                  ctx <- many1 letter_ascii
+                  return ctx
+
+parseContent :: Parser String
+parseContent = do c <- sepBy1 parseWord space
+                  return $ L.intercalate " " c
+    where
+        parseWord = do w <- many1 letter_ascii
+                       return w
