ffa338eec81ed916af5b8280ae3f0035a5ebb270
Aj <aj@aj.com>
2014-05-06 17:36:50 +0000

    converted the parsers for parsing project, priority and other parts of task from using monads to applicatives

--- todo.hs
+++ todo.hs
@@ -3,6 +3,7 @@ import Data.Time.Format
 import System.Locale
 import Data.Attoparsec.Combinator
 import Data.Attoparsec.ByteString.Char8
+import Control.Applicative
 import qualified Data.ByteString.Char8 as B
 import qualified Data.List as L
 
@@ -23,32 +24,27 @@ ct = "@weekend"
 test = "us doolars convert +makerspace"
 
 parsePriority :: Parser Priority
-parsePriority = do  char '('
-                    c <- satisfy $ inClass "ABC"
-                    char ')'
-                    return $ pri c
-    where
-        pri 'A' = A
-        pri 'B' = B
-        pri 'C' = C
+parsePriority = fmap pri $
+    char '(' *>
+    satisfy (inClass "ABC") <*
+    char ')' where
+        pri p = case p of
+            'A' -> A
+            'B' -> B
+            'C' -> C
 
 parseDate :: Parser UTCTime
-parseDate = do date <- count 10 $ choice [digit, char '-']
-               return $ (readTime defaultTimeLocale "%Y-%m-%d" date :: UTCTime)
+parseDate = (readTime defaultTimeLocale "%Y-%m-%d") <$>
+    (count 10 $ choice [digit, char '-'])
 
 parseProject :: Parser String
-parseProject = do char '+'
-                  prj <- many1 letter_ascii
-                  return prj
+parseProject = char '+' *>
+    many1 letter_ascii
 
 parseContext :: Parser String
-parseContext = do char '@'
-                  ctx <- many1 letter_ascii
-                  return ctx
+parseContext = char '@' *>
+    many1 letter_ascii
 
 parseContent :: Parser String
-parseContent = do c <- sepBy1 parseWord space
-                  return $ L.intercalate " " c
-    where
-        parseWord = do w <- many1 letter_ascii
-                       return w
+parseContent = L.intercalate " " <$> sepBy1 parseWord space where
+    parseWord = many1 letter_ascii
