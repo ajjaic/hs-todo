c5f1f6dc1ae741d5579fe64cc623cd621de6a85e
Aj <btchaggler@gmail.com>
2014-05-12 16:39:57 +0000

    content and context parser will now accept digits and characters

--- Tasktype.hs
+++ Tasktype.hs
@@ -1,23 +1,25 @@
 {-
-Defines the actual "Task" type.
+Defines the actual |Task| type.
 Contains internal functions used for parsing various
-parts of the "Task" type.
+parts of the |Task| type.
 -}
 
 module Tasktype(
     Priority(..),
     Task(..),
     parseTask,
---    pptask
+    readTaskFile,
 ) where
 
 import Data.Time.Clock
 import Data.Time.Format
-import System.Locale
+import System.Locale (defaultTimeLocale)
 import Data.Attoparsec.Combinator
 import Data.Attoparsec.ByteString.Char8
 import Control.Applicative
+import System.IO (withFile, IOMode(ReadMode))
 import qualified Data.List as L
+import qualified Data.ByteString.Char8 as B
 
 data Priority = A | B | C
 data Task = Task { priority :: Maybe Priority,
@@ -28,7 +30,7 @@ data Task = Task { priority :: Maybe Priority,
                    context :: [String] }
 
 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
--- Parser for parsing tasks
+-- Parser for parsing various components |Task|
 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
 parsePriority :: Parser Priority
 parsePriority = fmap pri
@@ -49,11 +51,11 @@ parseProject = char '+' *> many1 letter_ascii
 
 parseContent :: Parser String
 parseContent = L.intercalate " " <$> sepBy1 parseWord space where
-    parseWord = many1 letter_ascii
+    parseWord = many1 (letter_ascii <|> digit)
 
 parseContext :: Parser [String]
 parseContext = sepBy' context space where
-    context = char '@' *> many1 letter_ascii
+    context = char '@' *> many1 (letter_ascii <|> digit)
 
 parseTask :: Parser Task
 parseTask = Task
@@ -65,10 +67,8 @@ parseTask = Task
     <*> option [] parseContext
 
 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
--- Show instance for task
+-- Show instance for |Task|
 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
-
-
 instance Show (Priority) where
     show A = "(A)"
     show B = "(B)"
@@ -83,8 +83,29 @@ instance Show (Task) where
         pr = maybe "" id $ ('+':) <$> (project t)
         ct = L.intercalate " " $ map ('@':) $ context t
 
-pptask :: Task -> String
-pptask = show
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+-- Read |Task| from file
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+readTaskFile :: FilePath -> IO [Task]
+readTaskFile f = withFile f ReadMode helper where
+    helper h = do
+        bytes <- B.hGetContents h
+        let lines = B.lines bytes
+        return $ map getTask lines
+    getTask :: B.ByteString -> Task
+    getTask b = let getRight (Right t) = t
+                in getRight $ parseOnly parseTask b
+
+
+
+
+
+
+
+
+
+
+
 
 
 
