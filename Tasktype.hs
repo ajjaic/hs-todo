03f5ed25ed02350281f4301e592596fbf039842a
Aj <btchaggler@gmail.com>
2014-05-13 17:09:25 +0000

    basic repl loop in place

--- Tasktype.hs
+++ Tasktype.hs
@@ -18,6 +18,7 @@ import Data.Attoparsec.Combinator
 import Data.Attoparsec.ByteString.Char8
 import Control.Applicative
 import System.IO (withFile, IOMode(ReadMode))
+import System.Console.Haskeline
 import qualified Data.List as L
 import qualified Data.ByteString.Char8 as B
 
@@ -98,6 +99,96 @@ readTaskFile f = withFile f ReadMode helper where
     getTask :: B.ByteString -> Task
     getTask b = let getRight (Right t) = t
                 in getRight $ parseOnly parseTask b
+
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+-- REPL Commands
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+cmds = [("ls",   listTasks,   "List all the tasks"),
+        ("pv",   projectView, "List all tasks projectwise"),
+        ("cv",   contextView, "List all tasks contextwise"),
+        ("help", help,        "Show this help"),
+        ("at",   addTask,     "Create a new task"),
+        ("del",  delTask,     "Delete an existing task"),
+        ("app",  appTask,     "Append to an existing task")]
+
+listTasks :: [Task] -> String -> InputT IO ()
+listTasks t _ = mapM_ outputStrLn $ map show t
+
+projectView :: [Task] -> String -> InputT IO ()
+projectView tasks p = mapM_ outputStrLn
+    $ map show
+    $ filter (helper . project) tasks where
+        helper (Just prj) = prj == p
+        helper _ = False
+
+contextView = undefined
+help        = undefined
+addTask     = undefined
+delTask     = undefined
+appTask     = undefined
+
+
+execCmd :: [Task] -> (String, String) -> InputT IO ()
+execCmd t (c, args) = if L.null cmd'
+                        then outputStrLn ("WTF is: " ++ c)
+                        else let (_, cmd, _) = head cmd'
+                              in cmd t args
+    where
+        cmd' = filter (\(c',_,_) -> c == c') cmds
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+-- Helper functions
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+todoFile :: String
+todoFile = "todo.txt"
+
+prompt :: String
+prompt = ">> "
+
+blankLines :: Int -> IO ()
+blankLines n = putStr $ replicate n '\n'
+
+-- -- -- --  -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+-- REPL
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+
+oneREPloop :: [Task] -> InputT IO ()
+oneREPloop t = do
+    c <- getInputLine prompt
+    case c of
+        Nothing -> return ()
+        Just "" -> oneREPloop t
+        Just "quit" -> return ()
+        Just cmdargs -> do execCmd t $ mbreak cmdargs ""
+                           oneREPloop t
+    where
+        mbreak (x:xs) s = if isSpace x then (s, xs) else mbreak xs (s ++ [x])
+        mbreak [] s = (s, "")
+
+main :: IO ()
+main = do
+    tasks <- readTaskFile todoFile
+    runInputT defaultSettings $ oneREPloop tasks
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
