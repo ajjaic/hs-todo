1e30801216b6749e95dd8e18ba2b230955536ff2
Aj <btchaggler@gmail.com>
2014-05-23 03:47:16 +0000

    commands to handle projectview and context view

--- Tasktype.hs
+++ Tasktype.hs
@@ -1,15 +1,13 @@
-{-
-Defines the actual |Task| type.
-Contains internal functions used for parsing various
-parts of the |Task| type.
--}
+--Defines the actual |Task| type.
+--Contains internal functions used for parsing various
+--parts of the |Task| type.
 
-module Tasktype(
-    Priority(..),
-    Task(..),
-    parseTask,
-    readTaskFile,
-) where
+{-module Tasktype( -}
+{-    Priority(..),-}
+{-    Task(..),    -}
+{-    parseTask,   -}
+{-    readTaskFile,-}
+{-) where          -}
 
 import Data.Time.Clock
 import Data.Time.Format
@@ -117,61 +115,76 @@ data Command = Command {name :: String,
                         func :: Maybe String -> StateT Sessionstate (InputT IO) (),
                         desc :: String}
 
-ls = Command "ls" listTasks "List all the tasks"
-cmds = [ls]
-
-{-cmds = [("ls",   listTasks,   "List all the tasks")]-}
-        {-("lsp",  listProjects,"List all the projects"),     -}
-        {-("lsc",  listContexts,"List all the contexts"),     -}
-        {-("pv",   projectView, "List all tasks projectwise"),-}
-        {-("cv",   contextView, "List all tasks contextwise"),-}
-        {-("help", help,        "Show this help"),            -}
-        {-("at",   addTask,     "Create a new task"),         -}
-        {-("del",  delTask,     "Delete an existing task"),   -}
-        {-("app",  appTask,     "Append to an existing task")]-}
-
-{-listTasks :: [Task] -> Maybe String -> InputT IO ()                      -}
-{-listTasks t _ = mapM_ outputStrLn $ map show t                           -}
-
-{-projectView :: [Task] -> Maybe String -> InputT IO ()                    -}
-{-projectView tasks Nothing = mapM_ allprjs $ lsprojects tasks where       -}
-{-    allprjs p = outputStrLn (makeProject p) >> projectView tasks (Just p)-}
-{-projectView tasks p = mapM_ outputStrLn                                  -}
-{-    $ map show                                                           -}
-{-    $ filter (helper . project) tasks where                              -}
-{-        helper prj = prj == p                                            -}
-
-{-contextView :: [Task] -> Maybe String -> InputT IO ()                    -}
-{-contextView t Nothing = mapM_ allctxts $ lscontexts t where              -}
-{-    allctxts c = outputStrLn (makeContext c) >> contextView t (Just c)   -}
-{-contextView t (Just c) = mapM_ outputStrLn                               -}
-{-    $ map show                                                           -}
-{-    $ filter (helper . context) t where                                  -}
-{-        helper = any (==c)                                               -}
-
-{-listProjects :: [Task] -> Maybe String -> InputT IO ()                   -}
-{-listProjects t _ = mapM_ outputStrLn $ lsprojects t                      -}
-
-{-listContexts :: [Task] -> Maybe String -> InputT IO ()                   -}
-{-listContexts t _ = mapM_ outputStrLn $ lscontexts t                      -}
-
-{-help        = undefined                                                  -}
-{-addTask     = undefined                                                  -}
-{-delTask     = undefined                                                  -}
-{-appTask     = undefined                                                  -}
+ls   = Command   "ls"     listTasks      "List all the tasks"
+lsp  = Command   "lsp"    listProjects   "List all the projects"
+lsc  = Command   "lsc"    listContexts   "List all the contexts"
+pv   = Command   "pv"     projectView    "List all tasks projectwise"
+cv   = Command   "cv"     contextView    "List all tasks contextwise"
+help = Command   "help"   todoHelp           "Show this help"
+at   = Command   "at"     addTask        "Create a new task"
+del  = Command   "del"    deleteTask     "Delete an existing task"
+app  = Command   "app"    appTask        "Append to an existing task"
+cmds = [ls, lsp, lsc, pv, cv, help, at, del, app]
 
 
-{-execCmd :: [Task] -> (String, Maybe String) -> InputT IO ()              -}
-{-execCmd t (c, args) = getcmd where                                       -}
-{-    getcmd                                                               -}
-{-        | L.null cmd' = outputStrLn (unknownCmd ++ c)                    -}
-{-        | otherwise = cmd t args                                         -}
-{-    cmd' = filter (\(c',_,_) -> c == c') cmds                            -}
-{-    (_,cmd, _) = head cmd'                                               -}
+todoHelp    = undefined
+addTask     = undefined
+deleteTask  = undefined
+appTask     = undefined
+
+contextView :: Maybe String -> StateT Sessionstate (InputT IO) ()
+contextView (Just c) = do
+    ss <- get
+    let t = M.elems $ tasks ss
+        ctxt = filter ((any (==c)) . context) t
+    if L.null ctxt
+        then return ()
+        else do lift $ outputStrLn (makeContext c)
+                    >> mapM_ (outputStrLn . show) ctxt
+contextView _ = do
+    ss <- get
+    let c = contexts ss
+        (c', c'') = (init c, last c)
+        c'v = mapM_ (\cc -> contextView cc  >> lift (outputStrLn "")) (map Just c')
+        c''v = contextView (Just c'')
+    if length c == 1
+        then contextView (Just (head c))
+        else c'v >> c''v
+
+
+projectView :: Maybe String -> StateT Sessionstate (InputT IO) ()
+projectView prj@(Just p) = do
+    ss <- get
+    let t    = M.elems $ tasks ss
+        prjt = filter ((==prj) . project) t
+    if L.null prjt
+        then return ()
+        else do lift $ outputStrLn (makeProject p)
+                    >> mapM_ (outputStrLn . show) prjt
+projectView _ = do
+    ss <- get
+    let p = projects ss
+        (p', p'') = (init p, last p)
+        p'v = mapM_ (\pp -> projectView pp  >> lift (outputStrLn "")) (map Just p')
+        p''v = projectView (Just p'')
+    if length p == 1
+        then projectView (Just $ head p)
+        else p'v >> p''v
+
+
 listTasks :: Maybe String -> StateT Sessionstate (InputT IO) ()
 listTasks _ = get
     >>= (\s -> lift $ mapM_ (outputStrLn . show) $ M.elems $ tasks s)
 
+listProjects :: Maybe String -> StateT Sessionstate (InputT IO) ()
+listProjects _ = do
+    ss <- get
+    lift $ mapM_ (outputStrLn . makeProject) $ projects ss
+
+listContexts :: Maybe String -> StateT Sessionstate (InputT IO) ()
+listContexts _ = do
+    ss <- get
+    lift $ mapM_ (outputStrLn . makeContext) $ contexts ss
 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
 -- Helper functions
 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
@@ -180,12 +193,6 @@ todoFile = "todo.txt"
 
 prompt :: String
 prompt = ">> "
-
-unknownCmd :: String
-unknownCmd = "WTF is: "
-
-blankLines :: Int -> IO ()
-blankLines n = putStr $ replicate n '\n'
 
 lsprojects :: [Task] -> [String]
 lsprojects t = L.nub $ catMaybes $ map project t
@@ -199,6 +206,7 @@ makeProject p = "+" ++ p
 makeContext :: String -> String
 makeContext c = "@" ++ c
 
+
 lookupFunction :: String -> Maybe (Maybe String -> StateT Sessionstate (InputT IO) ())
 lookupFunction cmd =  if L.null cmd' then Nothing else Just $ func $ head cmd' where
     cmd' = filter (\c -> (name c) == cmd) cmds
@@ -207,53 +215,40 @@ lookupFunction cmd =  if L.null cmd' then Nothing else Just $ func $ head cmd' w
 -- REPL
 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
 
-{-oneREPloop :: [Task] -> InputT IO ()                                              -}
-{-oneREPloop t = do                                                                 -}
-{-    c <- getInputLine prompt                                                      -}
-{-    case c of                                                                     -}
-{-        Nothing -> return ()                                                      -}
-{-        Just "" -> oneREPloop t                                                   -}
-{-        Just "quit" -> return ()                                                  -}
-{-        Just cmdargs -> do execCmd t $ mbreak cmdargs ""                          -}
-{-                           oneREPloop t                                           -}
-{-    where                                                                         -}
-{-        mbreak :: String -> String -> (String, Maybe String)                      -}
-{-        mbreak (x:xs) s = if isSpace x then (s, Just xs) else mbreak xs (s ++ [x])-}
-{-        mbreak [] s = (s, Nothing)                                                -}
-
---main :: IO ()
---main = do
---    tasks <- readTaskFile todoFile
---    let ss =
---    runInputT defaultSettings $ oneREPloop tasks
-
+-- TODO:Perhaps it would be better if we can have a seperate session
+-- field to store the tasks as a list as well.
 data Sessionstate = Sessionstate { tasks    :: M.Map Int Task,
                                    projects :: [String],
                                    contexts :: [String]} deriving (Show)
 
 main :: IO ()
 main = do
+-- TODO:What if the tasks are empty?
     tasks <- readTaskFile todoFile
-    let taskmap = M.fromAscList $ zip [1 ..] tasks
+    let taskmap  = M.fromAscList $ zip [1 ..] tasks
         projects = lsprojects tasks
         contexts = lscontexts tasks
-        ss = Sessionstate taskmap projects contexts
+        ss       = Sessionstate taskmap projects contexts
     runInputT defaultSettings $ evalStateT oneREPloop ss
 
 oneREPloop :: StateT Sessionstate (InputT IO) ()
 oneREPloop = do c <- lift $ getInputLine prompt
                 return ()
                 case c of
-                    Nothing -> return ()
-                    Just "" -> oneREPloop
-                    Just "quit" -> return ()
+                    Nothing      -> return ()
+                    Just ""      -> oneREPloop
+                    Just "quit"  -> return ()
+                    Just "exit"  -> return ()
                     Just cmdargs -> let (cmd, args) = mbreak cmdargs ""
                                         cmd' = lookupFunction cmd
-                                     in if isNothing cmd' then oneREPloop else (fromJust cmd') args
+                                     in if isNothing cmd'
+                                            then unknowcmd cmd
+                                            else applycmd (fromJust cmd') args
     where
-        mbreak :: String -> String -> (String, Maybe String)
         mbreak (x:xs) s = if isSpace x then (s, Just xs) else mbreak xs (s ++ [x])
-        mbreak [] s = (s, Nothing)
+        mbreak [] s     = (s, Nothing)
+        applycmd c args = (c args) >> oneREPloop
+        unknowcmd c     = (lift $ outputStrLn ("WTF is: " ++ c)) >> oneREPloop
 
 
 
