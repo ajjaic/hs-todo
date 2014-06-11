d1bfb315c097ba1d5428343ccbda43f6ff6211ba
Aj <btchaggler@gmail.com>
2014-06-11 08:45:01 +0000

    removed unwanted code from main function

--- Main.hs
+++ Main.hs
@@ -13,32 +13,24 @@ import System.Console.Haskeline.Completion (simpleCompletion, completeWord)
 import Task
 import Commands
 
-data Runstatus = Continue | Stop | Payload (Maybe String)
-
 main :: IO ()
 main = do
     eithererrortasks <- tryIOError (readTaskFile todoFile)
     either ioError startrepl eithererrortasks where
-
         startrepl tasks = do
             let taskmap  = toMap tasks
                 projects = lsProjects tasks
                 contexts = lsContexts tasks
                 ss       = Sessionstate taskmap projects contexts
-                runonce = do
-                    ps <- runInputT defaultSettings $ evalStateT oneREPloop ss
-                    case ps of
-                        Continue -> runonce
-                        Stop -> return ()
-            runonce
+            runInputT defaultSettings $ evalStateT oneREPloop ss
 
-oneREPloop :: StateT Sessionstate (InputT IO) Runstatus
+oneREPloop :: StateT Sessionstate (InputT IO) ()
 oneREPloop = do c <- lift $ getInputLine prompt
                 case c of
-                    Nothing      -> return Stop
-                    Just ""      -> return Continue
-                    Just "quit"  -> return Stop
-                    Just "exit"  -> return Stop
+                    Nothing      -> return ()
+                    Just ""      -> oneREPloop
+                    Just "quit"  -> return ()
+                    Just "exit"  -> return ()
                     Just cmdargs -> parsedinp cmdargs
     where
         parsedinp c = case parseOnly parseInput (B.pack c) of
